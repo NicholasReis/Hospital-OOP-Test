f2001f905c711a0581424ff5c28afef0c20450cd
Nicholas <NicholasCReis@gmail.com>
2021-11-10 16:42:22 -0800

    Creating a shell of an idea
    Outlining an idea, this may become a glorified database. This may also become a randomized simulation. For now this is what seems necessary, but we will see if I need to add or remove things as I work.

---
+++ Hospital.java
@@ -0,0 +1,13 @@
+public class Hospital{
+    public static void main(String[] args){
+        System.out.println("Works");
+    }
+    
+    public static void addDoctor(){
+        
+    }
+    
+    public static void addPatient(){
+        
+    }
+}
---
+++ Specialization.java
@@ -0,0 +1,5 @@
+public class Specialization{
+    public Specialization(){
+        
+    }
+}
