d6ff2048ca8f769f4f8876da1762bbac8b4bff2c
Nicholas <NicholasCReis@gmail.com>
2021-11-10 17:17:18 -0800

    Fleshed out Patient and Added symptom.java
    Patient should have all relevant information now. May add some actions if this becomes a sim. Symptoms list will be used to assess which doctors should be seeing the patient.

--- Patient.java
+++ Patient.java
@@ -1,5 +1,57 @@
+import java.util.ArrayList;
+
 public class Patient{
-    public Patient(){
-        
+    //IDs rather than doctor name/room name for physical security reasons
+    private int doctorID;
+    private String roomCode;
+    protected String firstName;
+    protected String lastName;
+    protected char sex;
+    protected char gender;
+    protected int age;
+    protected ArrayList<Symptom> symptoms;
+    public Patient(int doctorID, String roomCode, String firstName,
+    String lastName, char sex, char gender, int age, ArrayList<Symptom> symptoms){
+        this.doctorID = doctorID;
+        this.roomCode = roomCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.symptoms = symptoms;
+    }
+
+    public int getPatientDoctorID(){
+        return doctorID;
+    }
+
+    public String getPatientRoomCode(){
+        return roomCode;
+    }
+
+    public String getPatientName(){
+        //Prioritizes preferred gender, should one not be provided defaults to sex
+        if(Character.toLowerCase(gender) == 'm'){
+            return "Mr. "+ lastName;
+        }else if(Character.toLowerCase(gender) == 'f'){
+            return "Ms. "+ lastName;
+        }else if(Character.toLowerCase(gender) == 'n'){
+            return "Mx. "+ lastName;
+        }else{
+            if(sex == 'm'){
+                return "Mr. "+ lastName;
+            }else if(sex == 'f'){
+                return "Ms. "+ lastName;
+            }else{
+                return "Mx. "+ lastName;
+            }
+        }
+    }
+    
+    public int getPatientAge(){
+        return age;
+    }
+    
+    public ArrayList<Symptom> getPatientSymptoms(){
+        return symptoms;
     }
 }
---
+++ Symptom.java
@@ -0,0 +1,5 @@
+public class Symptom{
+
+    public Symptom(){
+    }
+}
