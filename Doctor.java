c235e793c916499318d746d644bc98791346f976
Nicholas <NicholasCReis@gmail.com>
2021-11-12 13:49:41 -0800

    Created the Doctor Class
    Pretty self explanatory. Has some mutator methods. I decided not to have specializations specifically connected to the doctor and let the hospital class (or a new class altogether) control the flow of information. No reason to go through the doctor class to get info on different specializations, same goes for patients and symptoms. Will make those adjustments next.

--- Doctor.java
+++ Doctor.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+
 public class Doctor{
-    public Doctor(){
-        
+    protected int id;
+    protected String firstName;
+    protected String lastName;
+    protected int age;
+    protected ArrayList<String> specialization = new ArrayList<String>();
+    protected String office;
+    protected ArrayList<String> patients = new ArrayList<String>();
+    /*
+     * Future additions may include schedule/availability, salary, etc
+     * This is fine for now though.
+     */
+
+    public Doctor(int id, String firstName, String lastName, int age,
+    String office){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.specialization = specialization;
+        this.office = office;
+    }
+
+    public String getName(){
+        return "Dr. " + lastName;
+    }
+
+    public ArrayList<String> getSpecialization(){
+        return specialization;
+    }
+
+    public ArrayList<String> getPatients(){
+        return patients;
+    }
+
+    public void addSpecialization(String discipline){
+        specialization.add(discipline);
+    }
+
+    public void addPatient(String patientName){
+        patients.add(patientName);
     }
 }
