2603c86937264fd2a05fd8fe7229912e9bd87fb2
Nicholas <NicholasCReis@gmail.com>
2021-11-24 14:56:42 -0800

    Added Building and Room functionality
    This is still likely to change, but I got it off the ground. Going to start organizing what connects with what a little more clearly in the coming pushes.

--- Building.java
+++ Building.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Building{
-    public Building(){
-        
+    protected String name;
+    protected char code;
+    protected ArrayList<Room> roomList = new ArrayList<Room>();
+    
+    public Building(String name, char code){
+        this.name = name;
+        this.code = code;
+    }
+    
+    public void addRoom(char roomNumber, int roomCapacity){
+        roomList.add(new Room(roomNumber, roomCapacity));
+    }
+    
+    public void addRoom(Room r){
+        roomList.add(r);
+    }
+    
+    //Not sure what key I will use here to identify rooms
+    public String getRoomCode(Room r){
+        return (code + "-" + r.getRoomNumber());
     }
 }
--- Room.java
+++ Room.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+
 public class Room{
-    public Room(){
-        
+    protected int roomNumber;
+    protected int roomCapacity;
+    protected ArrayList<Patient> patientList = new ArrayList<Patient>();
+    
+    public Room(int roomNumber, int roomCapacity){
+        this.roomNumber = roomNumber;
+        this.roomCapacity = roomCapacity;
+    }
+
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+    
+    public boolean getRoomAvailability(){
+        if(patientList.size() < roomCapacity){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void addPatient(Patient patient){
+        if(getRoomAvailability()){
+            patientList.add(patient);
+        }else{
+            System.out.println("Room is full");
+        }
+    }
+
+    //Will have to test this later
+    public void removePatient(int patientID){
+        for(Patient p : patientList){
+            if(p.getId() == patientID){
+                patientList.remove(p);
+            }
+        }
     }
 }
